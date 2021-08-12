1cd3905e44c43dcb55a55284bb365c78ade6d775
Nathan Rowe <nathanrowe@bell.net>
2021-08-12 13:09:55 -0400

    Document fzf-git with comments

--- fzf-git.zsh
+++ fzf-git.zsh
@@ -3,8 +3,10 @@ is_in_git_repo() {
 }
 
 fzf_git_branch() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
+  # Pipe commands to fuzzy-search and select single branch
   git branch --color=always --all --sort=-committerdate |
     grep -Ev "HEAD|remote" |
     fzf --ansi --no-multi --preview-window right:65% --header "Select a branch" \
@@ -13,8 +15,10 @@ fzf_git_branch() {
 }
 
 fzf_git_branch_multi() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
+  # Pipe commands to fuzzy-search and select multiple branches
   git branch --color=always --all --sort=-committerdate |
     grep -Ev "HEAD|remote" |
     fzf --ansi --multi --preview-window right:65% --header "Select a branch" \
@@ -23,14 +27,17 @@ fzf_git_branch_multi() {
 }
 
 fzf_git_commit_hash() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
+  # Pipe commands to fuzzy-search and select git commit
   git log --decorate --pretty=oneline |
     fzf |
     awk '{print $1}'
 }
 
 fzf_git_diff_name_only() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
   git diff --name-only |
@@ -43,6 +50,7 @@ fzf_git_diff_name_only() {
 }
 
 fzf_git_unmerged_files() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
   git diff --name-only --diff-filter=U |
@@ -55,6 +63,7 @@ fzf_git_unmerged_files() {
 }
 
 fzf_git_merge_conflicts() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
   local file=$(fzf_git_unmerged_files)
@@ -82,113 +91,128 @@ fzf_git_merge_conflicts() {
 }
 
 fzf_git_diff() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  local mod_files
-
-  mod_files=$(fzf_git_diff_name_only)
+  local mod_files=$(fzf_git_diff_name_only)
 
   if [[ "$mod_files" = "" ]]; then
     echo "No file(s) selected."
     return
   fi
 
+  # git diff of selected modified file(s)
   echo $mod_files | xargs git diff
 }
 
 fzf_git_overwrite_local() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  local mod_files
-
-  mod_files=$(fzf_git_diff_name_only)
+  local mod_files=$(fzf_git_diff_name_only)
 
   if [[ "$mod_files" = "" ]]; then
     echo "No file(s) selected."
     return
   fi
 
+  # Overwrite local changes to selected modified file(s)
   echo $mod_files | xargs git checkout --
 }
 
 # TODO: Add flag to include remote branches
 fzf_git_checkout() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  local branch
+  local branch=$(fzf_git_branch)
 
-  branch=$(fzf_git_branch)
   if [[ "$branch" = "" ]]; then
       echo "No branch selected."
       return
   fi
 
   if [[ "$branch" = "remotes/"* ]]; then
+    # Checkout branch and set to tracking remote
     git checkout --track $branch
   else
+    # Checkout local branch
     git checkout $branch
   fi
 }
 
 fzf_git_delete_branch() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  branches=$(fzf_git_branch_multi)
+  local branches=$(fzf_git_branch_multi)
+
   if [[ "$branches" = "" ]]; then
     echo "No branch(es) selected."
       return
   fi
 
+  # Delete selected branch(es)
   echo $branches | xargs git branch -d
 }
 
 fzf_git_force_delete_branch() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  branches=$(fzf_git_branch_multi)
+  local branches=$(fzf_git_branch_multi)
+
   if [[ "$branches" = "" ]]; then
     echo "No branch(es) selected."
     return
   fi
 
+  # Force delete selected branch(es)
   echo $branches | xargs git branch -D
 }
 
 fzf_git_merge() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  branch=$(fzf_git_branch)
+  local branch=$(fzf_git_branch)
+
   if [[ "$branch" = "" ]]; then
     echo "No branch selected."
     return
   fi
 
+  # Merge selected branch
   git merge $branch
 }
 
 fzf_git_rebase() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  branch=$(fzf_git_branch)
+  local branch=$(fzf_git_branch)
+
   if [[ "$branch" = "" ]]; then
     echo "No branch selected."
     return
   fi
 
+  # Rebase selected branch
   git rebase $branch
 }
 
 fzf_git_rebase_interactive() {
+  # Return if function not called from git repo
   is_in_git_repo || return
 
-  local commit_hash
+  local commit_hash=$(fzf_git_commit_hash)
 
-  commit_hash=$(fzf_git_commit_hash)
   if [[ "$commit_hash" = "" ]]; then
     echo "No commit selected."
     return
   fi
 
+  # Interactive rebase using selected commit as fork point
   git rebase -i $commit_hash
 }
 
